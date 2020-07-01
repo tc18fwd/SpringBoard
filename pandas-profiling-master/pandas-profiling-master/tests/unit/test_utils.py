2b3c6c4b58acd6efb17330674eab49fe7564a5d0
Tom Cheng <tc18fwd@gmail.com>
2020-07-01 02:23:02 -0700

    capstone 2 step 2 completed

---
+++ pandas-profiling-master/pandas-profiling-master/tests/unit/test_utils.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+import pandas as pd
+import pytest
+
+from pandas_profiling.utils.dataframe import expand_mixed, read_pandas, warn_read
+
+
+def test_read_pandas_parquet():
+    p = Path("dataframe.parquet")
+    with pytest.raises(OSError) as e:
+        read_pandas(p)
+
+    assert str(e.value) in [
+        # pyarrow
+        "Passed non-file path: dataframe.parquet",
+        # fastparquet
+        "[Errno 2] No such file or directory: 'dataframe.parquet'",
+    ]
+
+
+def test_read_pandas_csv():
+    p = Path("dataframe.csv")
+    with pytest.raises(OSError) as e:
+        read_pandas(p)
+
+    assert str(e.value) in [
+        "[Errno 2] File b'dataframe.csv' does not exist: b'dataframe.csv'",
+        "[Errno 2] File dataframe.csv does not exist: 'dataframe.csv'",
+    ]
+
+
+def test_read_pandas_json():
+    p = Path("dataframe.json")
+    with pytest.raises(ValueError) as e:
+        read_pandas(p)
+
+    assert str(e.value) == "Expected object or value"
+
+
+def test_warning():
+    with pytest.warns(UserWarning):
+        warn_read("test")
+
+
+def test_expand():
+    df = pd.DataFrame(data=[{"name": "John", "age": 30}, {"name": "Alice", "age": 25}])
+    expanded_df = expand_mixed(df)
+    assert expanded_df.shape == (2, 2)
